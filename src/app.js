5bfdb28ab91f16651b562e13271f010cf5b17336
Szász Benedek <benedekszasz7@gmail.com>
2026-04-29 13:58:00 +0200

    Könnyű elemek láthatóak a táblázatban

--- src/app.js
+++ src/app.js
@@ -3,34 +3,50 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
 
 
-
 const elementList = [
-  { nev: 'Hidrogén', vegyjel: 'H', rendszam: 1, atomtomeg: 1.008, allapot: 'Gáz' },
-  { nev: 'Hélium', vegyjel: 'He', rendszam: 2, atomtomeg: 4.003, allapot: 'Gáz' },
-  { nev: 'Lítium', vegyjel: 'Li', rendszam: 3, atomtomeg: 6.94, allapot: 'Szilárd' },
-  { nev: 'Szén', vegyjel: 'C', rendszam: 6, atomtomeg: 12.011, allapot: 'Szilárd' },
-  { nev: 'Nitrogén', vegyjel: 'N', rendszam: 7, atomtomeg: 14.007, allapot: 'Gáz' },
-  { nev: 'Oxigén', vegyjel: 'O', rendszam: 8, atomtomeg: 15.999, allapot: 'Gáz' },
-  { nev: 'Fluor', vegyjel: 'F', rendszam: 9, atomtomeg: 18.998, allapot: 'Gáz' },
-  { nev: 'Neon', vegyjel: 'Ne', rendszam: 10, atomtomeg: 20.180, allapot: 'Gáz' }
+    { nev: 'Hidrogén', vegyjel: 'H', rendszam: 1, atomtomeg: 1.008, allapot: 'Gáz' },
+    { nev: 'Hélium', vegyjel: 'He', rendszam: 2, atomtomeg: 4.003, allapot: 'Gáz' },
+    { nev: 'Lítium', vegyjel: 'Li', rendszam: 3, atomtomeg: 6.94, allapot: 'Szilárd' },
+    { nev: 'Szén', vegyjel: 'C', rendszam: 6, atomtomeg: 12.011, allapot: 'Szilárd' },
+    { nev: 'Nitrogén', vegyjel: 'N', rendszam: 7, atomtomeg: 14.007, allapot: 'Gáz' },
+    { nev: 'Oxigén', vegyjel: 'O', rendszam: 8, atomtomeg: 15.999, allapot: 'Gáz' },
+    { nev: 'Fluor', vegyjel: 'F', rendszam: 9, atomtomeg: 18.998, allapot: 'Gáz' },
+    { nev: 'Neon', vegyjel: 'Ne', rendszam: 10, atomtomeg: 20.180, allapot: 'Gáz' }
 ]
 
 const doc = {
-  tbody: document.querySelector('#tbody')
+    tbody: document.querySelector('#tbody'),
+    resultDiv: document.querySelector('#result')
 }
 
-var rows = "";
-for (const elem of elementList) {
-  console.log(elem.nev)
-  var row = `
+function render(elemek) {
+    var rows = "";
+    for (const elem of elementList) {
+        console.log(elem.nev)
+        var text = "";
+        if(elem.rendszam < 5){
+            text = "Könnyű elem"
+        }
+        var row = `
     <tr>
-      <td>${elem.nev}</td>
+      <td>${elem.nev} <span class="badge text-bg-success">${text}</span></td>
       <td>${elem.vegyjel}</td>
       <td>${elem.rendszam}</td>
       <td>${elem.atomtomeg}</td>
       <td>${elem.allapot}</td>
     </tr>`
-  rows += row;
+        rows += row;
+    }
+    doc.tbody.innerHTML = rows;
 }
 
-doc.tbody.innerHTML = rows;
+function renderCountedValues() {
+    let count = 0;
+    for(elem of elemek){
+        if(elem.allapot == "Gáz"){
+            count++;
+        }
+    }
+    doc.resultDiv.innerHTML = count
+}
+render(elementList);
